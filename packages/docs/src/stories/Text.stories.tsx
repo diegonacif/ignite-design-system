f15c7ceb2415a3a6e3c5791784b9e76fa35f4953
diegonacif <diegoreisnacif@gmail.com>
2023-07-20 16:08:09 -0300

    feat: component text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, vitae voluptates optio, unde maxime atque, mollitia eius provident earum facilis repudiandae. Ad repudiandae eveniet veritatis dolorum a architecto veniam necessitatibus?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
