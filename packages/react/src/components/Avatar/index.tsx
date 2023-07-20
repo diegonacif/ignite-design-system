c638c5b67f9080ad9cf6b46f57f3afdedc02c2ff
diegonacif <diegoreisnacif@gmail.com>
2023-07-20 17:07:04 -0300

    feat: avatar component

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,18 @@
+import { User } from '@phosphor-icons/react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+import { ComponentProps, ElementType } from 'react'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {
+  as?: ElementType
+}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
